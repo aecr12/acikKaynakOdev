5dfca999cf222dc9a10469ba70654fc8635b7677
mhmmdbircan <100215553+mhmmdbircan@users.noreply.github.com>
2022-12-16 14:39:13 +0300

    Update Ornek.java
    Bubble Sort eklendi

--- Ornek.java
+++ Ornek.java
@@ -36,4 +36,19 @@ public static void merge(
     while (j < right) {
         a[k++] = r[j++];
     }
+    //Bubble Sort
+    
+    
+    void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
 }
